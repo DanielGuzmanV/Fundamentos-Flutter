8eab422f6d608a087993def56e755f1ce81a083f
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-02-05 11:23:26 -0400

    Configuracion de las rutas del drawer y tabs

---
+++ lib/config/router/app_router.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import 'package:fundamentos_flutter/features/home/presentation/screens/home_screen.dart';
+import 'package:fundamentos_flutter/features/concepts/presentation/screens/basic_concepts_screen.dart';
+import 'package:fundamentos_flutter/features/concepts/presentation/screens/intermediate_concepts_screen.dart';
+import 'package:fundamentos_flutter/features/concepts/presentation/screens/advanced_concepts_screen.dart';
+import 'package:fundamentos_flutter/features/examples/presentation/screens/examples_screen.dart';
+import 'package:fundamentos_flutter/features/tests/presentation/screens/tests_screen.dart';
+import 'package:fundamentos_flutter/features/apis/presentation/screens/apis_screen.dart';
+import 'package:fundamentos_flutter/shared/main_wrapper.dart';
+
+final _rootNavigatorKey = GlobalKey<NavigatorState>();
+
+final appRouter = GoRouter(
+  initialLocation: '/',
+  navigatorKey: _rootNavigatorKey,
+
+  routes: [
+    // Rutas que llevan al envoltorio principal
+    StatefulShellRoute.indexedStack(
+      builder: (context, state, navigationShell) {
+        return MainWrapper(navigationShell: navigationShell);
+      },
+      branches: [
+        // Rama 1: home (solo esta el Drawer)
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: '/',
+              builder: (context, state) => const HomeScreen(),
+            )
+          ]
+        ),
+
+        // Rama 2: Conceptos (Esta tendra las Tabs)
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: '/concepts/basic',
+              builder: (context, state) => const BasicConceptsScreen(),
+            ),
+            GoRoute(
+              path: '/concepts/intermediate',
+              builder: (context, state) => const IntermediateConceptsScreen(),
+            ),
+            GoRoute(
+              path: '/concepts/advanced',
+              builder: (context, state) => const AdvancedConceptsScreen(),
+            )
+          ]
+        ),
+
+        // Rama 3: Ejemplos
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: '/examples',
+              builder: (context, state) => const ExamplesScreen(),
+            ),
+          ],
+        ),
+
+        // Rama 4: Pruebas
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: '/tests',
+              builder: (context, state) => const TestsScreen(),
+            ),
+          ],
+        ),
+
+        // Rama 5: APIs
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: '/apis',
+              builder: (context, state) => const ApisScreen(),
+            ),
+          ],
+        ),
+      ]
+    )
+  ]
+);
