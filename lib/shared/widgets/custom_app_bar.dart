7f1186748442447acfe7efa4850ad3465d5c31d2
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-03-20 12:23:09 -0400

    Creacion de un widget para el appbar global

---
+++ lib/shared/widgets/custom_app_bar.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:fundamentos_flutter/config/router/app_router.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  final List<Widget>? actions;
+
+  const CustomAppBar({super.key, required this.title, this.actions});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text(title),
+      actions: actions,
+      leading: IconButton(
+        icon: const Icon(Icons.menu),
+        onPressed: () => mainScaffoldKey.currentState?.openDrawer(),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
