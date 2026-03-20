22b886aab246a4d1ff9142fcef635b7842a02b1d
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-03-20 11:14:06 -0400

    Creacion de barril de exportaciones para el approuter

---
+++ lib/features/features_exports.dart
@@ -0,0 +1,7 @@
+export 'package:fundamentos_flutter/features/home/presentation/screens/home_screen.dart';
+export 'package:fundamentos_flutter/features/concepts/presentation/screens/basic_concepts_screen.dart';
+export 'package:fundamentos_flutter/features/concepts/presentation/screens/intermediate_concepts_screen.dart';
+export 'package:fundamentos_flutter/features/concepts/presentation/screens/advanced_concepts_screen.dart';
+export 'package:fundamentos_flutter/features/examples/presentation/screens/examples_screen.dart';
+export 'package:fundamentos_flutter/features/tests/presentation/screens/tests_screen.dart';
+export 'package:fundamentos_flutter/features/settings/presentation/screens/settings_screen.dart';
