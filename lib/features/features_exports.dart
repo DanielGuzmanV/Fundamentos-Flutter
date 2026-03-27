be1d3f477948c900c32bdbe2b43ee0d2b92d5d76
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-03-27 11:11:02 -0400

    Se corrigio las importaciones de demos

--- lib/features/features_exports.dart
+++ lib/features/features_exports.dart
@@ -1,5 +1,5 @@
 export 'package:fundamentos_flutter/features/home/presentation/screens/home_screen.dart';
-export 'package:fundamentos_flutter/features/concepts/presentation/screens/basic_concepts_screen.dart';
-export 'package:fundamentos_flutter/features/concepts/presentation/screens/intermediate_concepts_screen.dart';
+export 'package:fundamentos_flutter/features/demos/presentation/screens/project_one_screen.dart';
+export 'package:fundamentos_flutter/features/demos/presentation/screens/project_two_screen.dart';
 export 'package:fundamentos_flutter/features/examples/presentation/screens/examples_screen.dart';
 export 'package:fundamentos_flutter/features/settings/presentation/screens/settings_screen.dart';
