1a8aeb2aa8d367874077f44316246cd4b0736f52
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-03-27 11:10:21 -0400

    Se renombre la carpeta y archivos de conceptos por demos

---
+++ lib/features/demos/presentation/screens/project_one_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ProjectOneScreen extends StatelessWidget {
+  const ProjectOneScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('Pantalla para el proyecto 1'));
+    
+  }
+}
---
+++ lib/features/demos/presentation/screens/project_two_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProjectTwoScreen extends StatelessWidget {
+  const ProjectTwoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('Pantalla para el proyecto 2'));
+  }
+}
