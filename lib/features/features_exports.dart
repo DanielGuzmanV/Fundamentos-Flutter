8084078ff0dfec6b354ebe8eed0178637aff06f4
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-04-03 11:47:16 -0400

    Se ajustaron las importaciones

--- lib/features/features_exports.dart
+++ lib/features/features_exports.dart
@@ -1,5 +1,9 @@
+// Pantallas del drawer y bottomBar
 export 'package:fundamentos_flutter/features/home/presentation/screens/home_screen.dart';
-export 'package:fundamentos_flutter/features/demos/presentation/screens/project_one_screen.dart';
-export 'package:fundamentos_flutter/features/demos/presentation/screens/project_two_screen.dart';
+export 'package:fundamentos_flutter/features/demos/insta_feed/presentation/screens/presentation_insta_feed.dart';
+export 'package:fundamentos_flutter/features/demos/task_flow/presentation/screens/presentation_task_flow.dart';
 export 'package:fundamentos_flutter/features/examples/presentation/screens/examples_screen.dart';
 export 'package:fundamentos_flutter/features/settings/presentation/screens/settings_screen.dart';
+
+// Pantallas completas
+export 'package:fundamentos_flutter/features/demos/insta_feed/presentation/screens/insta_feed_screen.dart';
