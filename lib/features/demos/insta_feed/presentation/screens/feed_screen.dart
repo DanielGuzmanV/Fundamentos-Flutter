acf15457ba6b32368a00cb2fdf3a051dfbb080a8
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-04-10 17:15:19 -0400

    Se crearon las pantallas principales para el insta feed

--- lib/features/demos/insta_feed/presentation/screens/feed_screen.dart
+++ lib/features/demos/insta_feed/presentation/screens/feed_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-class InstaFeedScreen extends StatelessWidget {
-  const InstaFeedScreen({super.key});
+class FeedScreen extends StatelessWidget {
+  const FeedScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
---
+++ lib/features/demos/insta_feed/presentation/screens/search_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SearchScreen extends StatelessWidget {
+  const SearchScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // Este proyecto tendrá su propia lógica de navegación
+      
+      body: const Center(child: Text('Pantalla para las busquedas')),
+    );
+  }
+}
