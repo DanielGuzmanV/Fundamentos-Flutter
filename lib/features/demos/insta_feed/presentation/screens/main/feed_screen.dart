ee8024f9144a22b19cf1abcfdbe969f74833dbe4
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-05-09 15:22:53 -0400

    Refactorizacion de la seccion de historias usando el nuevo widget

--- lib/features/demos/insta_feed/presentation/screens/main/feed_screen.dart
+++ lib/features/demos/insta_feed/presentation/screens/main/feed_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fundamentos_flutter/features/demos/insta_feed/presentation/widgets/main/historias_list.dart';
 
 class FeedScreen extends StatelessWidget {
   const FeedScreen({super.key});
@@ -23,7 +24,7 @@ class FeedScreen extends StatelessWidget {
 
           // Seccion de historias:
           SliverToBoxAdapter(
-            child: _HistoriasList()
+            child: HistoriasList()
           ),
 
           // El feed de posts:
@@ -42,55 +43,6 @@ class FeedScreen extends StatelessWidget {
           )
         ],
       )
-    );
-  }
-}
-
-class _HistoriasList extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 110,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-            child: Column(
-              children: [
-                Container(
-                  width: 65,
-                  height: 65,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    gradient: const LinearGradient(
-                      colors: [
-                        Colors.yellow,
-                        Colors.orange,
-                        Colors.red,
-                        Colors.purple,
-                      ],
-                      begin: Alignment.bottomLeft,
-                      end: Alignment.topRight,
-                    ),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(3), // Espacio para el gradiente
-                    child: CircleAvatar(
-                      backgroundColor: Colors.grey[300],
-                      child: Text('U-$index'),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 5),
-                Text('User $index', style: const TextStyle(fontSize: 12)),
-              ],
-            ),
-          );
-        },
-      ),
     );
   }
 }
