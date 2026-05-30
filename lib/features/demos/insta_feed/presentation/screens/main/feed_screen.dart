30b9c8536f4c391e5977a7513276e99aaf3233ad
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-05-30 16:00:05 -0400

    Se actualizo y cambio la pantalla principal a un consumer y usamos el provider

--- lib/features/demos/insta_feed/presentation/screens/main/feed_screen.dart
+++ lib/features/demos/insta_feed/presentation/screens/main/feed_screen.dart
@@ -1,47 +1,92 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fundamentos_flutter/features/demos/insta_feed/presentation/providers/providers.dart';
 import 'package:fundamentos_flutter/features/demos/insta_feed/presentation/widgets/main/historias_list.dart';
+import 'package:fundamentos_flutter/features/demos/insta_feed/presentation/widgets/main/post_card.dart';
 
-class FeedScreen extends StatelessWidget {
+class FeedScreen extends ConsumerWidget {
   const FeedScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    // Observar el estado del proveedor de posts
+    final feedPostsAsyncValue =  ref.watch(feedPostNotifierProvider);
+
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          // Barra superior:
-          const SliverAppBar(
-            floating: true,
-            title: Text(
-              'Insta Feed',
-              style: TextStyle(
-                fontFamily: 'Billabong',
-                fontSize: 26,
+      body: RefreshIndicator(
+        onRefresh: () => ref.read(feedPostNotifierProvider.notifier).refreshPost(),
+        child: CustomScrollView(
+          slivers: [
+            // Barra superior:
+            const SliverAppBar(
+              floating: true,
+              title: Text(
+                'Insta Feed',
+                style: TextStyle(
+                  fontFamily: 'Billabong',
+                  fontSize: 26,
+                ),
               ),
+              centerTitle: true,
             ),
-            centerTitle: true,
-          ),
+        
+            // Seccion de historias:
+            SliverToBoxAdapter(
+              child: HistoriasList()
+            ),
+        
+            // El feed de posts:
+            feedPostsAsyncValue.when(
+              loading: () => SliverToBoxAdapter(
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 20),
+                    child: CircularProgressIndicator(),
+                  ),
+                ),
+              ),
 
-          // Seccion de historias:
-          SliverToBoxAdapter(
-            child: HistoriasList()
-          ),
-
-          // El feed de posts:
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              childCount: 20,
-              (context, index) {
-                return Container(
-                  height: 300,
-                  margin: const EdgeInsets.all(10),
-                  color: Colors.grey[200],
-                  child: Center(child: Text('Post #$index')),
+              error: (err, stack) => SliverToBoxAdapter(
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Column(
+                      children: [
+                        Icon(Icons.error_outline, color: Colors.red, size: 40),
+                        SizedBox(height: 10),
+                        Text(
+                          'Error al cargar posts: $err',
+                          textAlign: TextAlign.center,
+                        ),
+                        ElevatedButton(
+                          onPressed: () => ref
+                              .read(feedPostNotifierProvider.notifier)
+                              .loadPosts(),
+                          child: Text('Reintentar'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              data: (posts) {
+                // 🔴 Si tenemos datos, mostramos la lista de posts
+                return SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                    childCount:
+                        posts.length, // Usamos la cantidad real de posts
+                    (context, index) {
+                      final post = posts[index];
+                      return PostCard(
+                        post: post,
+                      );
+                    },
+                  ),
                 );
               },
-            )
-          )
-        ],
+            ),
+          ],
+        ),
       )
     );
   }
