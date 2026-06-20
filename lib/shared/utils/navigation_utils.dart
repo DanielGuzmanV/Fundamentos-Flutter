81e2cb2470529b15e9a7cee4283d210127bd9122
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-06-20 08:57:34 -0400

    Se quitaron algunos metodos de la clase navigation utils

--- lib/shared/utils/navigation_utils.dart
+++ lib/shared/utils/navigation_utils.dart
@@ -2,19 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 class NavigationUtils {
-  // Logica para determinar el título de la barra superior
-  static String getTitle(int index) {
-    switch (index) {
-      case 0: return 'Inicio';
-      case 1: return 'Proyecto 1';
-      case 2: return 'Explorar Demos';
-      case 3: return 'Ajustes';
-      default: return '';
-    }
-  }
-
-  // Logica para saber si mostrar el BottomBar
-  static bool shouldShowBottomBar(int index) => index == 2;
 
   // Logica para centralizar las ruts de las tabs
   static void handleBottomNavClick(BuildContext context, int index) {
