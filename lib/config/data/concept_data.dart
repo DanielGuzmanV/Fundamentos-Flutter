f321ab69113fb86260a9f938a2d02dde954e6e6e
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-08-06 16:46:07 -0400

    Se creo la lista de conceptos para mapearlos

---
+++ lib/config/data/concept_data.dart
@@ -0,0 +1,13 @@
+import 'package:fundamentos_flutter/config/data/concept_item.dart';
+import 'package:fundamentos_flutter/config/data/concepts/advanced_concepts.dart';
+import 'package:fundamentos_flutter/config/data/concepts/basic_concepts.dart';
+import 'package:fundamentos_flutter/config/data/concepts/intermediate_concepts.dart';
+
+final List<ConceptCategory> conceptsList = [
+  // 1. CONCEPTOS BASICOS
+  basicConcepts,
+  // 2. CONCEPTOS INTERMEDIOS
+  intermediateConcepts,
+  // 3. CONCEPTOS AVANZADOS
+  advancedConcepts,
+];
