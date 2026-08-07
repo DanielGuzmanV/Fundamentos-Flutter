500890c8b3012d0214d1b70c756bf01f3a256a6c
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-08-06 22:11:40 -0400

    Se creo la ruta dinamica para cada tipo de concepto

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:fundamentos_flutter/config/data/concept_data.dart';
 import 'package:fundamentos_flutter/features/home/presentation/helpers/concept_card_mapppers.dart';
 import 'package:fundamentos_flutter/features/home/presentation/widgets/concept_card.dart';
 import 'package:fundamentos_flutter/features/home/presentation/widgets/home_header.dart';
+import 'package:go_router/go_router.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -28,7 +29,7 @@ class HomeScreen extends StatelessWidget {
             status: ConceptCardMappers.getStatusText(category.level), 
             statusColor: ConceptCardMappers.getStatusColor(category.level), 
             onTap: () {
-              // TODO: Navegar a la pantalla de detalle pasando `category` o `category.id`
+              context.go('/concepts/${category.id}');
             }
           );
         },
