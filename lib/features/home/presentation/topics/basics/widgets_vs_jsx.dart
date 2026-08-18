1d576b18f2bb70f549f3b955013a0658f63e3a3d
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-08-18 16:57:48 -0400

    Se creo una pantalla de prueba para el primer topic de concept basic

---
+++ lib/features/home/presentation/topics/basics/widgets_vs_jsx.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WidgetsVsJsx extends StatelessWidget {
+  const WidgetsVsJsx({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('Pantalla de (Widgets vs JSX RN)'));
+  }
+}
