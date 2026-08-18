b991ad867e07ad9e746f19cba46050821df83a61
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-08-18 17:58:59 -0400

    Refactorizacion de la clase navigation utils

--- lib/shared/utils/navigation_utils.dart
+++ lib/shared/utils/navigation_utils.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:fundamentos_flutter/config/router/app_routes_data.dart';
 import 'package:go_router/go_router.dart';
 
 class NavigationUtils {
 
-  // Logica para centralizar las ruts de las tabs
+  // Logica para centralizar las rutas de las tabs
   static void handleBottomNavClick(BuildContext context, int index) {
     switch (index) {
-      case 0: context.go('/demo/project-one'); break;
-      case 1: context.go('/demo/project-two'); break;
+      case 0: context.go(AppRoutes.projectOneDemoPath); break;
+      case 1: context.go(AppRoutes.projectTwoDemoPath); break;
     }
   }
 
   // Logica de seleccion de tabs para demos
   static int getBottomNavIndex(String location) {
-    if(location.contains('project-two')) return  1;
+    if(location.contains(AppRoutes.projectTwoDemoPath)) return  1;
     return 0;
   }
 
