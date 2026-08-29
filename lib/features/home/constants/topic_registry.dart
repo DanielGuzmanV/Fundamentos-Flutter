b67fc9478fb25993e809f4597031a92df12c3d57
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-08-28 23:04:48 -0400

    Se refactorizo el topic register para usar el nuevo componente

--- lib/features/home/constants/topic_registry.dart
+++ lib/features/home/constants/topic_registry.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fundamentos_flutter/features/home/presentation/topics/basics/widgets_vs_jsx.dart';
+import 'package:fundamentos_flutter/shared/widgets/topic_under_construction.dart';
 
 class TopicRegistry {
   static Widget getDemoForTopic(String topicId) {
@@ -7,18 +8,9 @@ class TopicRegistry {
       // Conceptos basicos
       case 'widgets-vs-jsx':
         return const WidgetsVsJsx();
-      
-      // Mejorar o crear un nuevo widget para el caso default 
+    
       default:
-        return const Center(
-          child: Padding(
-            padding: EdgeInsets.all(24.0),
-            child: Text(
-              'La demostracion para este concepto esta en desarrollo,',
-              textAlign: TextAlign.center,
-            ),
-          ),
-        );
+        return TopicUnderConstruction(topicId: topicId,);
     }
   }
 
